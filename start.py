ed24a82a0529f0234c7770eaaf5012a1791a75f9
dvss-dvss <kosevojdavid3@gmail.com>
2025-02-15 15:31:27 +0100

    Add background color

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import sys
 
-import pygame
+import pygame as pg
 
 
 class AlienInvasion:
@@ -8,21 +8,27 @@ class AlienInvasion:
 
     def __init__(self):
         """Инициализуэ гру та створюе игрови ресурси"""
-        pygame.init()
+        pg.init()
 
-        self.creen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.creen = pg.display.set_mode((1200, 800))
+        pg.display.set_caption("Alien Invasion")
+
+        # Призначення колиру фону
+        self.bg_color = (230, 230, 230) # RGB
 
     def run_game(self):
         """Запуск основного циклу гри"""
         while True:
             #Видслидкування подий клавиатури та миши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
                     sys.exit()
 
+            # За кожнойи интерацийи циклу оновлюэться екран
+            self.creen.fill(self.bg_color)
+
             # Видображення останнього прорисованого украну
-            pygame.display.flip()
+            pg.display.flip()
 
 if __name__ == '__main__':
     # Cndjhtyyz trptvgkzhe nf pfgecr uhb
