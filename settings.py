b2acab153dc3c612db6e6a69931a08c601ca326a
dvss-dvss <kosevojdavid3@gmail.com>
2025-03-01 15:03:41 +0100

    Add bullet mode

--- settings.py
+++ settings.py
@@ -8,5 +8,11 @@ class Settings:
        self.bg_color = (230, 230, 230)
 
        # Параметри корабля
-       self.ship_speed = 1.5
+       self.ship_speed = 5
+
+       # Параметри снаряду
+       self.bullet_speed = 1
+       self.bullet_width = 3
+       self.bullet_height = 15
+       self.bullet_color = (60, 60, 60)
        
