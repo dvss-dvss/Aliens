54b1a5849e9a0c2636307dee7fad2118fbd49920
dvss-dvss <kosevojdavid3@gmail.com>
2025-03-08 14:53:45 +0100

    Add Alien class

---
+++ alien.py
@@ -0,0 +1,22 @@
+import pygame as pg
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Класс для прибульця"""
+
+    def __init__(self, ai_game):
+        """Инициалицуе прибульця та задае його позицию"""
+        super().__init__()
+        self.creen = ai_game.creen
+
+        # Завантаження зображення та вызначення rect
+        self.image = pg.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
+
+        # Кошен новий прибулець з'являеться в ливому верхньому кути екрану
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # 
+        self.x = float(self.rect.x)
