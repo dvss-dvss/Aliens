cad18de824b9df6b30ba01ca8ac004678d282959
dvss-dvss <kosevojdavid3@gmail.com>
2025-03-22 15:20:37 +0100

    Add score fomatting

--- scoreboard.py
+++ scoreboard.py
@@ -21,7 +21,6 @@ class Scoreboard:
         """Перетворює поточний рахунок на графічне зображення"""
         round_score = round(self.stats.score, 1)
         score_str = f"{round_score:,}"
-        score_str = str(self.stats.score)
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
