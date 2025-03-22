7927954bb77bb487f6d73ccc727c661a57865685
dvss-dvss <kosevojdavid3@gmail.com>
2025-03-22 15:37:10 +0100

    Add high score

--- scoreboard.py
+++ scoreboard.py
@@ -14,8 +14,28 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pg.font.SysFont(None, 48)
 
-        # Підготовка початкового зображення
+        # Підготовка зображення
         self.prepare_score()
+        self.prepare_hight_score()
+
+    def check_high_score(self):
+        """Перевиряе, чи з'явився новий рекорд"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats .score
+            self.prepare_hight_score()
+
+    def prepare_hight_score(self):
+        """Перетворюе рекорд на графичне зображення"""
+        rounded_score = round(self.stats.high_score, -1)
+        score_str = f"{rounded_score}"
+        self.high_score_image = self.font.render(
+            score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        #
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
     def prepare_score(self):
         """Перетворює поточний рахунок на графічне зображення"""
@@ -33,3 +53,4 @@ class Scoreboard:
     def Show_score(self):
         """Виводе рахунок на екран"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
