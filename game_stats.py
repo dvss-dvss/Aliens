4828a5b1ad728fdecc434db133fedebe520f6ef7
dvss-dvss <kosevojdavid3@gmail.com>
2025-03-22 14:51:09 +0100

    Add Scoreboard class

--- game_stats.py
+++ game_stats.py
@@ -1,14 +1,16 @@
 class GameStats:
-     """Відслідковування статистики гри"""
+    """Відслідковування статистики гри"""
  
-     def __init__(self, ai_game):
-         """Ініціалізує статистику"""
-         self.settings = ai_game.settings
-         self.reset_stats()
+    def __init__(self, ai_game):
+        """Ініціалізує статистику"""
+        self.settings = ai_game.settings
+        self.reset_stats()
 
         # Гра запускається в неактивному стані
-         self.game_active = False
+        self.game_active = False
 
-     def reset_stats(self):
-         """Іеіціалізує статистику, що змінюється під час гри"""
-         self.ships_left = self.settings.ship_limit
+    def reset_stats(self):
+        """Іеіціалізує статистику, що змінюється під час гри"""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        
